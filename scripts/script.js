a3ffaf5bb385a5ee90ecdc336d8c504f31fbe286
PinkLaura <laura_nesossi@protonmail.com>
2021-10-20 19:33:57 +0200

    filter

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,43 @@
+function filterTags(t) {
+
+ /* var allTags;
+  var allTags = document.getElementsByClassName("pl-tag");
+ 
+  for(i=0; i< allTags.length; i++){
+
+    if (allProjects[i].className.indexOf(t) > -1) allProjects[i].classList.add("active");
+  }*/
+
+  const allTags = document.getElementsByClassName("pl-tag");
+
+
+  for(let i=0; i< allTags.length; i++){
+    allTags[i].classList.remove("active");
+
+    if (allTags[i].classList.contains(t)){
+      allTags[i].classList.add("active");
+    }
+  }
+
+
+  const allProjects = document.getElementsByClassName("project-div");
+
+  if(t == "all") t ="";
+
+
+  for(let i=0; i< allProjects.length; i++){
+    allProjects[i].classList.remove("project-show");
+
+    if (allProjects[i].className.indexOf(t) > -1) {
+      allProjects[i].classList.add("project-show");
+    }
+
+  }
+
+};
+
+
 $(document).ready(function() {
-
-
 
 // function to close the cv part on mobile
   $(function() {
@@ -12,24 +49,13 @@ $(document).ready(function() {
 
   });
 
-  filterTags("all");
+  const filter = window.location.search.slice(1).split("=")[1];
 
-  function filterTags(t) {
-    var allProjects, i;
-    allProjects = document.getElementsByClassName("project-div");
-
-    if(t == "all") t ="";
-
-    for(i=0; i< allProjects.length; i++){
-
-      allProjects[i].removeClass("project-show");
-      if (allProjects[i].className.indexOf(t) > -1) allProjects[i].addClass("project-show");
-    }
-  };
-
-
-
-
-
+  if(filter){
+    filterTags(filter);
+  }
+  else{    
+      filterTags("all");
+  }
 
 });
