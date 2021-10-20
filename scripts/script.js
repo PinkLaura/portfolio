0c10cf8269295eb67ee1f312d33b56c6d411d3e3
PinkLaura <laura_nesossi@protonmail.com>
2021-10-20 14:23:38 +0200

    All projects

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,22 @@ $(document).ready(function() {
 
   });
 
+  filterTags("all");
+
+  function filterTags(t) {
+    var allProjects, i;
+    allProjects = document.getElementsByClassName("project-div");
+
+    if(t == "all") t ="";
+
+    for(i=0; i< allProjects.length; i++){
+
+      allProjects[i].removeClass("project-show");
+      if (allProjects[i].className.indexOf(t) > -1) allProjects[i].addClass("project-show");
+    }
+  };
+
+
 
 
 
